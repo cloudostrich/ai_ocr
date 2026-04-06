de4c465cf18cb4b634732775bb993ceda4b015cf
cloudostrich <cloudostrich@gmail.com>
2026-04-06 12:42:28 +0800

    remove key

--- bakups/gemini/gemini_0.py
+++ bakups/gemini/gemini_0.py
@@ -1,6 +1,5 @@
 from google import genai
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY=AIzaSyB9pzcIfD6BoBlhBLR-ko5qDNYW_z36CHE`.
 client = genai.Client()
 
 response = client.models.generate_content(
